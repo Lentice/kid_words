7543035ac82dfee154af21f6516cbb95c7da603f
LenticeTsai <LenticeTsai@vianextech.com>
2025-11-15 23:26:15 +0800

    refactor: simplify progress data handling by removing unused properties

--- src/utils/progress.js
+++ src/utils/progress.js
@@ -19,20 +19,12 @@ export function getProgress() {
   return {
     learnedIds: new Set(d.learnedIds || []),
     lastIndex: Number.isInteger(d.lastIndex) ? d.lastIndex : 0,
-    selectedSectionIds: Array.isArray(d.selectedSectionIds) ? d.selectedSectionIds : [],
-    range: d.range || { start: 0, end: null },
-    wordSpeed: typeof d.wordSpeed === 'number' ? d.wordSpeed : 0.95,
-    exampleSpeed: typeof d.exampleSpeed === 'number' ? d.exampleSpeed : 0.95
   }
 }
 
-export function saveProgress({ learnedIds, lastIndex, selectedSectionIds, range, wordSpeed, exampleSpeed }) {
+export function saveProgress({ learnedIds, lastIndex }) {
   const data = read()
   if (learnedIds) data.learnedIds = Array.from(learnedIds)
   if (Number.isInteger(lastIndex)) data.lastIndex = lastIndex
-  if (Array.isArray(selectedSectionIds)) data.selectedSectionIds = selectedSectionIds
-  if (range) data.range = range
-  if (typeof wordSpeed === 'number') data.wordSpeed = wordSpeed
-  if (typeof exampleSpeed === 'number') data.exampleSpeed = exampleSpeed
   write(data)
 }
